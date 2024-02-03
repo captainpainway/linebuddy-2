ddd9f3ff8afebeb5ea6b043be1e0a99669575ae4
Mary Knize <maryknize@gmail.com>
2024-02-03 18:18:42 -0500

    Map components generated

--- process-maps/src/lib.rs
+++ process-maps/src/lib.rs
@@ -10,34 +10,77 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn process_map(text: String, width: f64, height: f64) -> js_sys::Array {
+pub fn process_nodes(text: String, width: f64, height: f64) -> js_sys::Array {
     let doc = osm::OSM::parse(text.as_bytes()).unwrap();
-    // log(&format!("{:?}", doc));
-
     let bounds = doc.bounds.unwrap();
     let arr = js_sys::Array::new();
+    for (_id, node) in doc.nodes.iter() {
+        arr.push(&process_points(node, &bounds, width, height));
+    }
+    arr
+}
 
+#[wasm_bindgen]
+pub fn process_ways(text: String, width: f64, height: f64) -> js_sys::Array {
+    let doc = osm::OSM::parse(text.as_bytes()).unwrap();
+    let bounds = doc.bounds.unwrap();
+    let arr = js_sys::Array::new();
     for (_id, way) in doc.ways.iter() {
-        let coords = js_sys::Array::new();
-                for node in way.nodes.iter() {
-                    let n = &doc.resolve_reference(&node);
-                    match n {
-                        osm::Reference::Node(node) => {
-                            let n_lat = map_points(node.lat, bounds.minlat, bounds.maxlat, 0.0, width);
-                            let n_lon = map_points(node.lon, bounds.minlon, bounds.maxlon, 0.0, height);
-                            let point = js_sys::Array::new();
-                            point.push(&JsValue::from_f64(n_lat));
-                            point.push(&JsValue::from_f64(n_lon));
-                            coords.push(&point);
+        arr.push(&process_coords(&doc, way, &bounds, width, height));
+    }
+    arr
+}
+
+#[wasm_bindgen]
+pub fn process_relations(text: String, width: f64, height: f64) -> js_sys::Array {
+    let doc = osm::OSM::parse(text.as_bytes()).unwrap();
+    let bounds = doc.bounds.unwrap();
+    let arr = js_sys::Array::new();
+    for (_id, relation) in doc.relations.iter() {
+        for member in relation.members.iter() {
+            match member {
+                osm::Member::Way(way, t) => {
+                    let w = &doc.resolve_reference(&way);
+                    match w {
+                        osm::Reference::Way(way) => {
+                            if t == "outer" {
+                                arr.push(&process_coords(&doc, way, &bounds, width, height));
+                            }
                         },
                         _ => {}
                     }
-                }
-        arr.push(&coords);
+                },
+                _ => {}
+            }
+        }
     }
-    return arr;
+    arr
+}
+
+fn process_coords(doc: &osm::OSM, way: &osm::Way, bounds: &osm::Bounds, width: f64, height: f64) -> js_sys::Array {
+    let coords = js_sys::Array::new();
+    for node in way.nodes.iter() {
+        let n = &doc.resolve_reference(&node);
+        match n {
+            osm::Reference::Node(node) => {
+                let point = process_points(node, &bounds, width, height);
+                coords.push(&point);
+            },
+            _ => {}
+        }
+    }
+    coords
+}
+
+fn process_points(node: &osm::Node, bounds: &osm::Bounds, width: f64, height: f64) -> js_sys::Array {
+    let n_lat = map_points(node.lat, bounds.minlat, bounds.maxlat, 0.0, width);
+    let n_lon = map_points(node.lon, bounds.minlon, bounds.maxlon, 0.0, height);
+    let point = js_sys::Array::new();
+    point.push(&JsValue::from_f64(n_lat));
+    point.push(&JsValue::from_f64(n_lon));
+    point
 }
 
 fn map_points(value: f64, start1: f64, stop1: f64, start2: f64, stop2: f64) -> f64 {
-    return ((value - start1) / (stop1 - start1) * (stop2 - start2) + start2).floor();
+    ((value - start1) / (stop1 - start1) * (stop2 - start2) + start2).floor()
 }
